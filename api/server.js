8fedd31e97ec419459d4e35f53a5cd73b1d4f594
PK <pradeepdhital124@gmail.com>
2023-02-12 12:07:01 +1100

    the application is changed into monolothic

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import path from "path";
 import { dbConnect } from "./src/config/dbconfig.js";
 const PORT = 8000;
 const app = express();
@@ -18,7 +19,9 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 // app.use(express.urlencoded())
-
+// static content serve
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 // ///// global error handling
 app.use((error, req, res, next) => {
   const status = error.status || 404;
@@ -31,8 +34,5 @@ app.use((error, req, res, next) => {
 // api end points
 app.use("/api/v1/task", taskRouter);
 app.use("/", (req, res) => {
-  res.json({
-    status: "success",
-    message: "response from the root url",
-  });
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
